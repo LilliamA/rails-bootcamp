1049d4a42034db4c88e4fcc434f5a0cafa663c45
LilliamA <lilliam.arce31@gmail.com>
2018-03-13 15:28:35 -0600

    Redireccionar a lista de proyectos desde actualizar proyecto

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
     before_action :set_projects, only: %i[index create destroy update]
+    after_action :set_tasks, only: %i[show ]
     def index
         @projects = Project.all
         respond_to do |format|
@@ -12,6 +13,7 @@ class ProjectsController < ApplicationController
     end
 
     def create
+        binding.pry
         @project = Project.new(project_params)
         @project.save
         respond_to do |format|
@@ -22,9 +24,7 @@ class ProjectsController < ApplicationController
     def update
          @project = Project.find(params[:id])
         if @project.update(project_params)
-           respond_to do |format|
-            format.js
-        end
+        redirect_to projects_path
         else
             render 'edit'
         end
@@ -36,10 +36,7 @@ class ProjectsController < ApplicationController
     
     def show
         @project = Project.find(params[:id])
-        @tasks = Task.where(project_id: params[:id])
-
         @task = Task.new
-        
         respond_to do |format|
             format.html
         end
@@ -56,6 +53,9 @@ class ProjectsController < ApplicationController
     private
     def set_projects
         @projects = Project.all
+    end
+    def set_tasks
+        @tasks = Task.where(project_id: params[:id])
     end
 
     def project_params
