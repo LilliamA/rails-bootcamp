00b5385420f226a31abc1878686779b74427dea8
SMVictor <victor.salvatierra@outlook.com>
2018-05-21 11:04:05 -0600

    .

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180315221016) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "text"
     t.integer "task_id"
@@ -39,7 +42,7 @@ ActiveRecord::Schema.define(version: 20180315221016) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.integer "priority"
-    t.integer "project_id"
+    t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_tasks_on_project_id"
@@ -66,4 +69,5 @@ ActiveRecord::Schema.define(version: 20180315221016) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "projects", on_delete: :cascade
 end
